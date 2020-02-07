1804348edd104be94f3f890bee8b85378815313b
frogcat <frogcat@users.noreply.github.com>
2020-02-07 18:48:06 +0900

    Add rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve';
 import buble from '@rollup/plugin-buble';
+import {
+  terser
+} from 'rollup-plugin-terser';
 
 export default {
   input: 'canvas-morph.js',
-  output: {
+  output: [{
     file: 'docs/canvas-morph.js',
     format: 'iife'
-  },
+  }, {
+    file: 'docs/canvas-morph.min.js',
+    format: 'iife',
+    plugins: [terser()]
+  }],
   plugins: [
     resolve(),
     buble()
